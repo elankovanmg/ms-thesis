a296cf868ce2c3111009adb182f61b0199a5656c
elankovanmg <5elangog@gmail.com>
2021-08-12 14:23:22 +0530

    initial commit

--- git_push.sh
+++ git_push.sh
@@ -7,7 +7,7 @@ cp index.md.html index.html
 
 ##git init main
 git add .
-git commit -m "New commit"
+git commit -m "initial commit"
 ## >>chmod 755 * // if required;
 ##git remote add origin https://github.com/elankovanmg/ms-thesis.git
 ##git remote -v
